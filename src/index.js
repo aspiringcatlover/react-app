575539c54e76ed15ef155b4f7a0d946df2d942d0
Hao Jie Tan <htan096@e.ntu.edu.sg>
2021-04-13 14:51:28 +0800

    First commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+
 ReactDOM.render(
+
+ 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
