fac6c569f73026fcac1f5203e661b17445b52ca5
Hao Jie Tan <htan096@e.ntu.edu.sg>
2021-04-14 18:02:23 +0800

    ee

--- src/VideoListComponent.js
+++ src/VideoListComponent.js
@@ -2,40 +2,42 @@
 
 import React, { useState, useEffect } from "react";
 import { DataGrid } from '@material-ui/data-grid';
-import requestNASFileList from '../routes/ApiCaller';
+import requestNASFileList from './routes/ApiCaller';
 
 
 const IncidentVideoResult = (props) => {
     const [axiosResult, setAxiosResult] = useState([]);
 
     useEffect(() => {
-        setAxiosResult = requestNASFileList(props.device_id);
+        setAxiosResult(requestNASFileList(props.device_id));
 
     }, [props.device_id]) //re-render only when device id changes
-
-    //return a data table to be rendered
-    const columns = [
-        {
-            headerName: 'File ID',
-            field: 'file_id',
-            width: 70
-        },
-        {
-            headerName: 'File Name',
-            field: 'file_name',
-            width: 130
-        },
-    ];
 
 
 
     console.log(axiosResult, "axiosResult");
-    if (axiosResult.length === 0) {
+    if (axiosResult === undefined ) {
         console.log("still waiting results from axios");
         return <>Still loading...</>
     }
     else {
         console.log("axios results ready");
+
+        //return a data table to be rendered
+
+        //columns for data table
+        const columns = [
+            {
+                headerName: 'File ID',
+                field: 'file_id',
+                width: 70
+            },
+            {
+                headerName: 'File Name',
+                field: 'file_name',
+                width: 130
+            },
+        ];
         return (
 
             <div style={{ height: 400, width: '100%' }}>
--- src/routes/ApiCaller.js
+++ src/routes/ApiCaller.js
@@ -5,19 +5,23 @@ let requestNASFileList = (device_id) => {
     //fetch response from nodejs express server
     axios.get("http://127.0.0.1:3001/nas-info?device_id=" + device_id).then((response) => {
 
+    
         //remove results with empty file_id
-        let withID = response.filter(function (el) {
+        let withID = (response.data.list).filter(function (el) {
             return el.file_id !== "";
         })
+        console.log("eeee",withID);
 
         //add a id key for data table to use
         for (let entries of withID) {
             entries.id = entries.file_id;
         }
 
+    
+        console.log("...",withID);
         return withID;
 
     })
 
 }
-export { requestNASFileList };
+export default requestNASFileList ;
